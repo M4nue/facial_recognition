a59cb2cc159a4434d7018d6c217faea34cfca242
M4nue <M4nue@users.noreply.github.com>
2024-07-29 20:26:02 +0200

    pruebas

--- pruebas.py
+++ pruebas.py
@@ -35,8 +35,26 @@ def ABRIR_CAMARA(camara):
     
     while True:
         ret, frame = cap.read()
-        cv2.imshow('IG: manuel_rc12', frame)
+        width = int(cap.get(3))
+        eight = int(cap.get(4))
+        
+        hvs = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) #FILTRO DIFERENCIAR COLORES
+
+        color_hsv_claro = np.array([0, 1, 1])   # Valores típicos inferiores para piel en HSV
+        color_hsv_oscuro = np.array([30, 255, 255])  # Ajusta estos valores según tus necesidades
+ 
+        mascara = cv2.inRange(hvs,color_hsv_claro,color_hsv_oscuro)#DETECCION DE COLORES EN X RANGO
+
+        resultado_img_mascara = cv2.bitwise_and(frame, frame, mask=mascara)
+
+        cv2.imshow('IG: manuel_rc12', resultado_img_mascara)
         if cv2.waitKey(1) == ord('q'):
             break
 
 ABRIR_CAMARA(0)
+
+
+
+
+
+
