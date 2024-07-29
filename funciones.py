f4e074e10cbc46355f3fabe1bf4efb0dcf02f467
M4nue <M4nue@users.noreply.github.com>
2024-07-29 18:30:36 +0200

    Creación  funcion abrir la camara

--- funciones.py
+++ funciones.py
@@ -1,8 +1,25 @@
 import cv2
 import random
-def imagen():
+import numpy as np
+def CARGAR_IMAGEN():
     img = cv2.imread('galeria/persona.jpg',-1) # Segundo argumento modifica la tonalidad 0=escalas de grises -1=normal
-    img = cv2.resize(img,(0,0),fx=0.7,fy=0.7)
+    img = cv2.resize(img,(0,0),fx=0.5,fy=0.5)
     return img
+
+def ABRIR_CAMARA(camara):
+    cap = cv2.VideoCapture(camara) #DEFINIMOS LA CAMARA
+    
+    while True:
+        ret, frame = cap.read()
+        cv2.imshow('IG: manuel_rc12', frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+    
+    cap.release()
+    cv2.destroyWindow()
+
+
+
+
 
 
